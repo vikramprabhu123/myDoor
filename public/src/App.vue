27cad811c7e93acdcda9d8629abe3413e65868d2
vikramprabhu123 <vikramprabhu@gmail.com>
2020-12-25 19:39:28 -0500

    Vue

---
+++ public/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="garageImgContainer">
+      <img alt="Vue logo" class="garageImg" src="./assets/garagedoor.png">
+    </div>
+    <HelloWorld msg="Open"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+
+
+}
+
+.garageImgContainer{
+  padding: 5%;
+}
+.garageImg{
+  height: 90%;
+  width: 90%;
+  opacity: 0.5;
+}
+</style>
---
+++ public/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    
+    <button class="action">{{msg}}</button>
+
+    <h3>Door is closed</h3>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card{
+  position: absolute;
+  top: 5%;
+  padding-top: 35%;
+  width: 100%;
+}
+
+.action{
+  width: 80%;
+  min-height: 40px;
+  background-color: lightgreen;
+}
+
+h3 {
+  margin: 10px 0 0;
+  padding-top: 30px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
